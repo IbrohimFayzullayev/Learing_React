132f2c5d7d98f1bf46abd4a8b82a4f3cd153ca95
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-20 10:11:15 +0500

    second day

---
+++ src/CommentComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react";
+import faker from "@faker-js/faker";
+
+const CommentComponent = (props) => {
+  // props bu yerda obyekt kiradi
+  return (
+    <div className="ui comments">
+      <div className="comment">
+        <a className="avatar">
+          <img src={props.img}></img>
+        </a>
+        <div className="content">
+          <a href="#" className="author">
+            {props.name}
+          </a>
+          <div className="metadata">
+            <div className="date">{props.day} days ago</div>
+            <div className="rating">
+              <i className="star icon"></i>5 Faves
+            </div>
+          </div>
+          <div className="text">{props.comment}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CommentComponent;
